ae3f87f36b0cd5684079293666869deffa735003
kwlahr <kwlahr@gmail.com>
2020-03-04 20:45:13 -0700

    restructured filepaths for sequelize and sequelized the models and routes. Added passport vor validation

--- models/user.js
+++ models/user.js
@@ -13,15 +13,51 @@
 //     PRIMARY KEY (id)
 // );
 
+// module.exports = function(sequelize, DataTypes) {
+//   var User = sequelize.define("User", {
+//     first_name: DataTypes.STRING,
+//     last_name: DataTypes.STRING,
+//     password: DataTypes.STRING,
+//     age: DataTypes.INTEGER,
+//     skill_level: DataTypes.INTEGER,
+//     instrument: DataTypes.STRING,
+//     genre: DataTypes.STRING
+//   });
+//   return User;
+// };
+
+// Requiring bcrypt for password hashing. Using the bcryptjs version as the regular bcrypt module sometimes causes errors on Windows machines
+var bcrypt = require("bcryptjs");
+// Creating our User model
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
     first_name: DataTypes.STRING,
     last_name: DataTypes.STRING,
-    password: DataTypes.STRING,
     age: DataTypes.INTEGER,
     skill_level: DataTypes.INTEGER,
     instrument: DataTypes.STRING,
-    genre: DataTypes.STRING
+    genre: DataTypes.STRING,
+    // The email cannot be null, and must be a proper email before creation
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    // The password cannot be null
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
+  // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
+  User.prototype.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+  };
+  User.addHook("beforeCreate", function(user) {
+    user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
   });
   return User;
 };
